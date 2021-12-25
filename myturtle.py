8be57d73b41977b42128bc6c025054db4fb10e7c
zehwakaka <1985682903@qq.com>
2021-12-25 21:51:44 +0800

    session01

--- myturtle.py
+++ myturtle.py
@@ -1,9 +1,92 @@
 import tkinter
 from turtle import *
-forward(100)
+'''first code block'''
+# forward(100)
+# shape('turtle')
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+
+
+'''second code block'''
+# for i in range(2):
+#     print("hello world")
+
+# for i in range(10):
+#     print("hello world")
+
+# for i in range(10):
+#     print(i)
+
+'''third code block'''
+# shape('turtle')
+# for i in range(4):
+#     forward(100)
+#     right(90)
+
+'''fourth code block'''
+# shape('turtle')
+# def square():
+#     for i in range(4):
+#         forward(100)
+#         right(90)
+# square()
+
+'''fifth code block'''
+# shape('turtle')
+# def square():
+#     for j in range(60):
+#         right(5)
+#         for i in range(4):
+#             forward(100)
+#             right(90)
+# square()
+'''sixth code block'''
+# def square(sidelength=80):
+#     for i in range(4):
+#         forward(sidelength)
+#         right(90)
+
+# square(50)
+# square(30)
+# square()
+'''seventh code block'''
+# def triangle(sidelength=100):
+#     for i in range(3):
+#         forward(sidelength)
+#         right(120)
+#     left(120)
+#     forward(sidelength)
+# triangle(90)
+'''eighth code block'''
+# def triangle(sidelength=100):
+#     for i in range(3):
+#         forward(sidelength)
+#         right(60)
+# triangle()
+'''ninth code block'''
+# shape('turtle')
+# def square():
+#     length = 0
+#     for j in range(60):
+#         length += 5
+#         right(5)
+#         for i in range(4):
+#             forward(length)
+#             right(90)
+# square()
+'''ten code block'''
 shape('turtle')
-right(45)
-forward(150)
+def star():
+    for i in range(5):
+        forward(100)
+        left(216)
+star()
 
 
 
+def starSpiral():
+    pass
